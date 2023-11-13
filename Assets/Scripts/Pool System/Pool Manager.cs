e9ab52817d96e2d39a49a9659ffafff568626739
1145141919 <1820219936@qq.com>
2023-11-13 14:04:38 +0800

    Update Pool Manager.cs

--- Assets/Scripts/Pool System/Pool Manager.cs
+++ Assets/Scripts/Pool System/Pool Manager.cs
@@ -1,8 +1,15 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PoolManager : MonoBehaviour
 {
      [SerializeField] private Pool[] playerProjectilePools;      // 存放玩家发射物体的对象池数组
+     static private Dictionary<GameObject, Pool> dictionary;
+
+     private void Awake()
+     {
+          dictionary = new Dictionary<GameObject, Pool>();
+     }
 
      private void Start()
      {
@@ -17,21 +24,162 @@ public class PoolManager : MonoBehaviour
      {
           foreach (var pool in pools)   // 历遍数组的所有物体
           {
+
+          #if UNITY_EDITOR
+               if (dictionary.ContainsKey(pool.Prefab))
+               {
+                    Debug.LogError("有重复的预制体：" + pool.Prefab);
+                    continue;
+               }
+          #endif
+
+               dictionary.Add(pool.Prefab, pool);
                // 为每个对象池创建一个父级 Transform，并命名为 "Pool: 预制体名称"
                Transform poolParent = new GameObject("Pool: " + pool.Prefab.name).transform;
                poolParent.parent = transform;     // 将新创建的父级 Transform 设置为当前对象的子对象
                pool.Initialize(poolParent);       // 调用对象池的初始化方法，将新创建的父级 Transform 传入进行对象池的初始化
           }
      }
-     /*
+
+     #region 释放对象
+
      /// <summary>
-     /// 
+     /// <para>Return a specified <paramref name="prefab"></paramref> gameObject in the pool.</para>
+     /// <para>根据传入的 <paramref name="prefab"></paramref> 参数，返回对象池中预备好的游戏对象。</para>
      /// </summary>
-     /// <param name="prefab"></param>
-     /// <returns></returns>
+     /// 
+     /// <param name="prefab">
+     /// <para>specified gameObject prefab</para>
+     /// <para>指定的游戏对象预制体</para>
+     /// </param>
+     /// 
+     /// <returns>
+     /// <para>Prepared gameObject in the pool</para>
+     /// <para>对象池中预备好的游戏对象</para>
+     /// </returns>
      public static GameObject Release(GameObject prefab)
      {
+          #if UNITY_EDITOR
+          if (!dictionary.ContainsKey(prefab))
+          {
+               Debug.LogError("对象池管理器找不到预制件：" + prefab.name);
 
+               return null;
+          }
+          #endif
+          return dictionary[prefab].PreparedObject();
      }
-     */
+
+     /// <summary>
+     /// <para>Return a specified <paramref name="prefab"></paramref> gameObject in the pool.</para>
+     /// <para>根据传入的 <paramref name="prefab"></paramref> 参数，返回对象池中预备好的游戏对象。</para>
+     /// </summary>
+     /// 
+     /// <param name="prefab">
+     /// <para>Specified gameObject prefab</para>
+     /// <para>指定的游戏对象预制体</para>
+     /// </param>
+     /// 
+     /// <param name="position">
+     /// <para>Specified gameObject position</para>
+     /// <para>指定释放位置</para>
+     /// </param>
+     /// 
+     /// <returns>
+     /// <para>Prepared gameObject in the pool</para>
+     /// <para>对象池中预备好的游戏对象</para>
+     /// </returns>
+     public static GameObject Release(GameObject prefab, Vector2 position)
+     {
+          #if UNITY_EDITOR
+          if (!dictionary.ContainsKey(prefab))
+          {
+               Debug.LogError("没有找到预制体：" + prefab.name);
+
+               return null;
+          }
+          #endif
+          return dictionary[prefab].PreparedObject(position);
+     }
+
+     /// <summary>
+     /// <para>Return a specified <paramref name="prefab"></paramref> gameObject in the pool.</para>
+     /// <para>根据传入的 <paramref name="prefab"></paramref> 参数，返回对象池中预备好的游戏对象。</para>
+     /// </summary>
+     /// 
+     /// <param name="prefab">
+     /// <para>Specified gameObject prefab</para>
+     /// <para>指定的游戏对象预制体</para>
+     /// </param>
+     /// 
+     /// <param name="position">
+     /// <para>Specified gameObject position</para>
+     /// <para>指定释放位置</para>
+     /// </param>
+     /// 
+     /// <param name="rotation">
+     /// <para>Specified gameObject rotation</para>
+     /// <para>指定释放角度</para>
+     /// </param>
+     /// 
+     /// <returns>
+     /// <para>Prepared gameObject in the pool</para>
+     /// <para>对象池中预备好的游戏对象</para>
+     /// </returns>
+     public static GameObject Release(GameObject prefab, Vector2 position, Quaternion rotation)
+     {
+          #if UNITY_EDITOR
+          if (!dictionary.ContainsKey(prefab))
+          {
+               Debug.LogError("没有找到预制体：" + prefab.name);
+
+               return null;
+          }
+          #endif
+          return dictionary[prefab].PreparedObject(position, rotation);
+     }
+
+     /// <summary>
+     /// <para>Return a specified <paramref name="prefab"></paramref> gameObject in the pool.</para>
+     /// <para>根据传入的 <paramref name="prefab"></paramref> 参数，返回对象池中预备好的游戏对象。</para>
+     /// </summary>
+     /// 
+     /// <param name="prefab">
+     /// <para>Specified gameObject prefab</para>
+     /// <para>指定的游戏对象预制体</para>
+     /// </param>
+     /// 
+     /// <param name="position">
+     /// <para>Specified gameObject position</para>
+     /// <para>指定释放位置</para>
+     /// </param>
+     /// 
+     /// <param name="rotation">
+     /// <para>Specified gameObject rotation</para>
+     /// <para>指定释放角度</para>
+     /// </param>
+     /// 
+     /// /// <param name="localScale">
+     /// <para>Specified gameObject localScale</para>
+     /// <para>指定释放方向</para>
+     /// </param>
+     /// 
+     /// <returns>
+     /// <para>Prepared gameObject in the pool</para>
+     /// <para>对象池中预备好的游戏对象</para>
+     /// </returns>
+     public static GameObject Release(GameObject prefab, Vector2 position, Quaternion rotation, Vector2 localScale)
+     {
+          #if UNITY_EDITOR
+          if (!dictionary.ContainsKey(prefab))
+          {
+               Debug.LogError("没有找到预制体：" + prefab.name);
+
+               return null;
+          }
+          #endif
+          return dictionary[prefab].PreparedObject(position, rotation, localScale);
+     }
+
+     #endregion
 }
